37ca6792e6231feb5f8062086aba1c8652ef694f
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-05-12 14:47:23 -0300

    login quase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
+import { Componente2Component } from './compoments/componente2/componente2.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Componente1Component } from './compoments/componente1/componente1.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
 
   ],
   providers: [],
