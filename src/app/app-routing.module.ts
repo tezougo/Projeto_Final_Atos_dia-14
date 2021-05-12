32a557357010626453e40496457771e7f2a03d84
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-05-12 16:22:24 -0300

    fazendo angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Componente1Component } from './compoments/componente1/componente1.component';
+import { Componente2Component } from './compoments/componente2/componente2.component';
 
 const routes: Routes = [
-
+  { path: 'componente1', component: Componente1Component },
+  { path: 'componente2', component: Componente2Component },
 ];
 
 @NgModule({
--- src/app/compoments/componente1/componente1.component.ts
+++ src/app/compoments/componente1/componente1.component.ts
@@ -11,5 +11,6 @@ export class Componente1Component implements OnInit {
 
   ngOnInit(): void {
   }
-
+  usuario: string;
+  senha: string;
 }
