35a203d39e67d0b7f0578d005c4997d5d1b35aff
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-05-12 21:28:32 -0300

    fazendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FileUploader } from 'ng2-file-upload/ng2-file-upload';
-
-const uploadAPI = 'http://localhost:4200/api/upload'; // better use a service class
 
 @Component({
   selector: 'app-root',
@@ -9,24 +6,8 @@ const uploadAPI = 'http://localhost:4200/api/upload'; // better use a service cl
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'ng8fileuploadexample';
-  public uploader: FileUploader = new FileUploader({
-    url: uploadAPI,
-    itemAlias: 'file',
-  });
+
   // tslint:disable-next-line:typedef
   ngOnInit() {
-    this.uploader.onAfterAddingFile = (file) => {
-      file.withCredentials = false;
-    };
-    this.uploader.onCompleteItem = (
-      item: any,
-      response: any,
-      status: any,
-      headers: any
-    ) => {
-      console.log('FileUpload:uploaded successfully:', item, status, response);
-      alert('Your file has been uploaded successfully');
-    };
+    }
   }
-}
