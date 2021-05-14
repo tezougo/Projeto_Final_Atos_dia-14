734e4414890d002bbe35358be33163c4f408723e
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-05-13 21:30:30 -0300

    cadastro_login_pronto

--- src/app/compoments/componente2/componente2.component.ts
+++ src/app/compoments/componente2/componente2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NewCadastro } from 'src/app/models/NewCadastro.model';
 import { Servico1Service } from 'src/app/services/servico1.service';
 
@@ -9,35 +10,36 @@ import { Servico1Service } from 'src/app/services/servico1.service';
 })
 export class Componente2Component implements OnInit {
   error: any;
-  constructor(private servico1: Servico1Service) {}
+  router: Router;
+  constructor(private servico1: Servico1Service, router: Router) {
+    this.router = router;
+  }
 
-  newcadastro: NewCadastro[];
+  newcadastro: boolean;
   usuario: string;
   senha: string;
   email: string;
+
   ngOnInit(): void {}
 
-  validaLogin() {
-    this.servico1.getLogin().subscribe(
-      (data: NewCadastro[]) => {
-        this.newcadastro = data;
-        console.log('O que retornou em dados "set": ', data);
-        console.log('Variavel cadastro: ', this.newcadastro);
-        if (this.newcadastro) {
-          for (let aux of this.newcadastro) {
-            if (this.usuario == aux.usuario && this.senha == aux.senha) {
-              console.log('Usuario validado');
+  adicionaUsuario() {
+    if (this.usuario == null || this.email == null || this.senha == null) {
+      this.newcadastro = false;
+    } else {
+      this.servico1
+        .postCadastro(this.usuario, this.email, this.senha)
+        .subscribe(
+          (data: boolean) => {
+            this.newcadastro = data;
+            if (this.newcadastro) {
+              this.router.navigate(['/login-page']);
             }
+          },
+          (error: any) => {
+            this.error = error;
+            console.log('Erro >> ' + this.error);
           }
-          this.servico1
-            .postCadastro(this.usuario, this.email, this.senha)
-            .subscribe();
-        }
-      },
-      (error: any) => {
-        this.error = error;
-        console.log('Erro: ' + this.error);
-      }
-    );
+        );
+    }
   }
 }
